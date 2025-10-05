370e5e42f72eff2cf9ecadf36ae14eb53d35e072
Rajaryan singh <raj.aryan2518@gmail.com>
2025-10-05 21:46:41 +0530

    added owner Schema

---
+++ models/owner.model.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const ownerSchema = mongoose.Schema(
+    {
+        username: {
+            type: String,
+            minLength: 3,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+         orders: {
+            type: Array,
+            default: []
+        },
+         contact: {
+            type: Number,
+        },
+        picture: {
+            type: String
+        },
+    },
+);
+
+module.exports = mongoose.model('Owner', ownerSchema);
