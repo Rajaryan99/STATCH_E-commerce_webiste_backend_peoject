2b6b93bdc433ca1e982b4b377c5c1ae7cec61f96
Rajaryan singh <raj.aryan2518@gmail.com>
2025-10-08 16:10:21 +0530

    encrypting password using bcrypt hash

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,16 +1,32 @@
 const express = require('express');
 const router = express.Router();
-const userModel = require('../models/user.model')
+const userModel = require('../models/user.model');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-router.post('/register', async (req, res) => {
+router.post('/register',  (req, res) => {
     try {
         let { username, email, password } = req.body;
-        let user = await userModel.create({
-            username,
-            email,
-            password
-        });
-        res.send(user);
+
+
+        bcrypt.genSalt(10, (err, salt) => {
+            if (err) return res.send(err.message);
+
+            bcrypt.hash(password, salt, async (err, hash) => {
+                if (err) return res.send(err.message);
+
+                else {
+                    let user = await userModel.create({
+                        username,
+                        email,
+                        password: hash,
+                    });
+                    jwt.sign()
+                }
+            })
+        })
+
+
     } catch (error) {
         res.send(error.message)
     }
